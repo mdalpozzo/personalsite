b24f9f33920ab82dc27034f175e621c8df5bb72f
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2019-01-29 21:19:28 -0800

    adds about content

--- client/src/comps/About.jsx
+++ client/src/comps/About.jsx
@@ -18,8 +18,8 @@ class About extends Component {
     highresimg.onload = () => {
       this.setState({
         mainpic: this.state.highres,
-      })
-    }
+      });
+    };
     highresimg.src = this.state.highres;
   }
 
@@ -33,17 +33,38 @@ class About extends Component {
             <h2> </h2>
           </div>
         </div>
-        <p className="aboutblurb">
-          I have experience building full-stack applications using Node, JavaScript, React, Redux,
-          Express and working with an array of databases from MongoDB to PostgreSQL.
-          <br />
-          <br />
-          I believe in always staying sharp and competitive as an engineer, and a human, by pushing
-          the limits of my comfort zone through constantly challenging myself and staying conscious
-          of innovation.
-          <br />
-          <br />I am always interested in delving into new technologies and furthering my knowledge.
-        </p>
+        <div className="aboutblurb">
+          <p>
+            <b>tl;dr {'{'}</b>
+            <br />
+            <span style={{ paddingLeft: '20px', display: 'block' }}>
+              I am a Software Engineer experienced in building full-stack web applications. I
+              specialize in building responsive, dynamic UIs on the front-end. I often choose
+              React/Redux components to ensure modular, maintainable code. For building fast and
+              reliable back-end systems I usually work with an Express server on Node.js. For quick
+              or simple database needs, I'll use MongoDB. If more complex data modeling is required,
+              I would opt for a SQL database such as PostgreSQL.
+            </span>
+            <b>{'}'}</b>
+          </p>
+          <p style={{ textIndent: '20px' }}>
+            I was born and raised in Santa Barbara, California. I moved to San Francisco in 2011
+            after studying Geology and Computer Science at Sonoma State University. After playing in
+            a rock band and making websites on the side I decided to turn full-time towards software
+            engineering. Eventually, in 2018 I completed the Hack Reactor Full-stack Engineering
+            Immersive bootcamp, sharpening my engineering toolset. I spent several months teaching
+            JavaScript fundamentals and React before deciding to pursue a more active engineering
+            career.
+            <br />
+            <br />
+            I currently freelance building web sites and web applications.
+            <br />
+            <br />I believe in always staying sharp and competitive as an engineer, and a human, by
+            pushing the limits of my comfort zone through constantly challenging myself and staying
+            conscious of innovation.
+            <br />
+          </p>
+        </div>
       </div>
     );
   }
